2953f435a55793e1661f1530e9f0db60447d88dd
Anna Miura <annamaliamiura@gmail.com>
2022-11-20 17:55:44 -1000

    refactor: Fix extra vertical scroll on pages

--- pages/crafts.tsx
+++ pages/crafts.tsx
@@ -13,8 +13,8 @@ export default function Crafts() {
   const cardMaxWidth = 345;
 
   return (
-    <Container maxWidth="lg" sx={{ height: "80vh" }}>
-      <Box sx={{ height: "100%" }}>
+    <Container maxWidth="lg" sx={{ height: "80vh", overflowY: "auto" }}>
+      <Box>
         <Grid
           alignItems="center"
           container
@@ -73,6 +73,25 @@ export default function Crafts() {
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
                   Completed September 21st, 2022
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+
+          <Grid item>
+            <Card sx={{ maxWidth: cardMaxWidth }}>
+              <CardMedia
+                component="img"
+                height="440"
+                image="https://i.imgur.com/f9h8e2a.jpg"
+                alt="space-dnd-themed-baby-blanket"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Baby Blanket - DnD & Space Themed
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Completed June 23rd, 2021
                 </Typography>
               </CardContent>
             </Card>
--- src/Copyright.tsx
+++ src/Copyright.tsx
@@ -1,15 +1,18 @@
+import Box from "@mui/material/Box";
 import MuiLink from "@mui/material/Link";
 import Typography from "@mui/material/Typography";
 
 export default function Copyright() {
   return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {"Copyright © "}
-      <MuiLink color="inherit" href="https://github.com/annaperdomo">
-        Variations on a String
-      </MuiLink>
-      &nbsp;
-      {new Date().getFullYear()}.
-    </Typography>
+    <Box sx={{ pt: 2 }}>
+      <Typography variant="body2" color="text.secondary" align="center">
+        {"Copyright © "}
+        <MuiLink color="inherit" href="https://github.com/annaperdomo">
+          Variations on a String
+        </MuiLink>
+        &nbsp;
+        {new Date().getFullYear()}.
+      </Typography>
+    </Box>
   );
 }
