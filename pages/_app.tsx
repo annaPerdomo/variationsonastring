147c8a9a158043048c59343e61993e192a540b72
Anna Miura <annamaliamiura@gmail.com>
2025-09-16 17:43:31 -0700

    fix: Update styling changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ import Head from "next/head";
 import createEmotionCache from "../src/styles/createEmotionCache";
 import Layout from "../src/components/Layout";
 import theme from "../src/styles/theme";
+import "../src/styles/styles.css";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
